fa6cf959d6aa91e760b3ca70b62359111a3e8345
AntonioBrivio <92523244+AntonioBrivio@users.noreply.github.com>
2024-08-09 17:38:58 -0300

    Restaurante Novo RecyclerView

--- RestauranteNovo/app/src/main/java/com/example/restaurantenovo/MainActivity.kt
+++ RestauranteNovo/app/src/main/java/com/example/restaurantenovo/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.restaurantenovo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.restaurantenovo.adapter.FoodAdapter
+import com.example.restaurantenovo.databinding.ActivityMainBinding
+import com.example.restaurantenovo.model.Food
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+    private lateinit var foodAdapter: FoodAdapter
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var foodList: MutableList<Food>
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        recyclerView = binding.recyclerFood
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        // Inicializar a lista de alimentos
+        foodList = mutableListOf(
+            Food(R.drawable.food1, "Prato Feito com Fritas", "Fritas e Frango", "20,00"),
+            Food(R.drawable.food2, "Sushi", "Os melhores rolls do bairro", "60,00"),
+            Food(R.drawable.food3, "Prato Feito Carne Assada", "Carne assada, farofa e torta salgada", "60,00")
+
+        // Adicione mais itens de comida aqui
+        )
+
+        // Configurar o adapter com a lista de alimentos
+        foodAdapter = FoodAdapter(this, foodList)
+        recyclerView.adapter = foodAdapter
     }
 }
--- RestauranteNovo/app/src/main/java/com/example/restaurantenovo/adapter/FoodAdapter.kt
+++ RestauranteNovo/app/src/main/java/com/example/restaurantenovo/adapter/FoodAdapter.kt
@@ -1,28 +1,33 @@
 package com.example.restaurantenovo.adapter
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.restaurantenovo.databinding.FoodItemBinding
 import com.example.restaurantenovo.model.Food
 
 class FoodAdapter(private val context: Context, private val foodList: MutableList<Food>): RecyclerView.Adapter<FoodAdapter.FoodViewHolder>(){
 
-    inner class FoodViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
-
+    inner class FoodViewHolder(binding: FoodItemBinding): RecyclerView.ViewHolder(binding.root){
+        val imgFood = binding.imgFood
+        val textFoodName = binding.textFoodName
+        val textFoodDescricao = binding.textFoodDescricao
+        val textFoodPrice = binding.textFoodPrice
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FoodViewHolder {
-        TODO("Not yet implemented")
+        val listItem: FoodItemBinding = FoodItemBinding.inflate(LayoutInflater.from(context),parent, false)
+        return FoodViewHolder(listItem)
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount() = foodList.size
 
     override fun onBindViewHolder(holder: FoodViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.imgFood.setBackgroundResource(foodList[position].getImagem())
+        holder.textFoodName.text = foodList[position].getNome()
+        holder.textFoodDescricao.text = foodList[position].getDescricao()
+        holder.textFoodPrice.text = foodList[position].getPreco()
     }
-
-
 }
