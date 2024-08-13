7c6e448581b71eda268c4efa67d80e1375bb741f
AntonioBrivio <92523244+AntonioBrivio@users.noreply.github.com>
2024-08-13 11:24:13 -0300

    Controle de Carros

---
+++ ControledeCarros/app/src/main/java/com/example/controledecarros/model/Carro.kt
@@ -0,0 +1,39 @@
+package com.example.controledecarros.model
+
+class Carro (
+    private var marca: String,
+    private var modelo: String,
+    private var ano: Int
+    ) {
+        // Getters e Setters estilo Java
+        fun getMarca(): String {
+            return marca
+        }
+
+        fun setMarca(marca: String) {
+            this.marca = marca
+        }
+
+        fun getModelo(): String {
+            return modelo
+        }
+
+        fun setModelo(modelo: String) {
+            this.modelo = modelo
+        }
+
+        fun getAno(): Int {
+            return ano
+        }
+
+        fun setAno(ano: Int) {
+            this.ano = ano
+        }
+
+        // Construtor
+        init {
+            println("Carro ${marca} ${modelo} criado.")
+        }
+
+    }
+
