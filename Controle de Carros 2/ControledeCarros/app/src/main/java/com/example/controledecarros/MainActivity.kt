417efd207a42d9c7a22b22c66c9187c816d605dd
AntonioBrivio <92523244+AntonioBrivio@users.noreply.github.com>
2024-08-14 17:01:44 -0300

    Controle de Carros 2

---
+++ Controle de Carros 2/ControledeCarros/app/src/main/java/com/example/controledecarros/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.controledecarros
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.controledecarros.databinding.ActivityMainBinding
+import com.example.controledecarros.model.Carro
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var carro: Carro
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Configurando o View Binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonCriar.setOnClickListener {
+            val marca = binding.editMarca.text.toString()
+            val modelo = binding.editModelo.text.toString()
+            val ano = binding.editAno.text.toString().toInt()
+
+            // Instanciando o objeto Carro
+            carro = Carro(marca, modelo, ano, 0)
+            mostrarDetalhes()
+        }
+
+        binding.buttonQuilometragem.setOnClickListener {
+            val novaQuilometragem = binding.editQuilometragem.text.toString().toInt()
+            carro.quilometragem = novaQuilometragem
+            mostrarDetalhes()
+        }
+    }
+
+    private fun mostrarDetalhes() {
+        val detalhes = """
+            Marca: ${carro.getMarca()}
+            Modelo: ${carro.getModelo()}
+            Ano: ${carro.getAno()}
+            Quilometragem: ${carro.quilometragem}
+        """.trimIndent()
+
+        binding.textDetalhes.text = detalhes
+    }
+}
